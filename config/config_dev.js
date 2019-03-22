c02ef8bf5f8c9765e4bad289d73c4dc0ffe20aa5
Paul <>
2019-03-22 10:47:45 +0800

    redis server

--- config/config_dev.js
+++ config/config_dev.js
@@ -3,8 +3,11 @@ const logPath = process.env.LOG_PATH;
 var config = {
 	appName: 'skyShared',
     redis: {
-        host: '10.211.55.5',
-        port: 6379
+        host: '10.20.32.250',
+		port: 6379,
+		options: {
+			db: 3
+		}
     },
     logger: {
         appenders: {
